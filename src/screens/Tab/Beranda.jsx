9871d1bdfacde49cfcfcdb743f4fb14f37236d8b
sam <rizal.victoria02@gmail.com>
2024-07-03 06:11:09 +0700

    first commit

--- src/screens/Tab/Beranda.jsx
+++ src/screens/Tab/Beranda.jsx
@@ -1,40 +1,12 @@
-import { StyleSheet, Text, View, Image } from 'react-native'
+import { View, Text } from 'react-native'
 import React from 'react'
 
-export default function Beranda() {
+const Beranda = () => {
   return (
-    <View style={styles.container}>
-      <Text style={[styles.text, { color: 'blue' }]}>Selamat Datang di Aplikasi Kami</Text>
-      <Image
-        source={require('../../assets/image/baground.jpg')}
-        style={styles.image}
-      />
-      <Image
-        source={require('../../assets/image/profil1.jpg')}
-        style={styles.image}
-      />
-      <Image
-        source={require('../../assets/image/foto.png')}
-        style={styles.image}
-      />
+    <View>
+      <Text>Beranda</Text>
     </View>
   )
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    borderRadius: 20,
-  },
-  image: {
-    width: 200,
-    height: 300,
-    resizeMode: 'contain',
-  },
-  text: {
-    fontSize: 24,
-    fontWeight: 'bold',
-  },
-})
+export default Beranda
--- src/screens/Tab/Pengaturan.jsx
+++ src/screens/Tab/Pengaturan.jsx
@@ -1,27 +1,12 @@
-import { StyleSheet, Text, View, Image } from 'react-native'
+import { View, Text } from 'react-native'
 import React from 'react'
 
-export default function Pengaturan() {
+const Pengaturan = () => {
   return (
-      <View style={styles.container}>
-        <Image
-        source={require('../../assets/image/baground.jpg')}
-        style={styles.image}
-        />
-      </View>
+    <View>
+      <Text>Pengaturan</Text>
+    </View>
   )
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    borderRadius: 20,
-  },
-  image: {
-    width: 200,
-    height: 300,
-    resizeMode: 'contain',
-  },
-})
+export default Pengaturan
